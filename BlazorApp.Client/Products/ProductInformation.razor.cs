9dff3b1043f984492ce3d4b70ebeed5d6f377bcd
NetDefender <danielsith@hotmail.com>
2023-12-20 00:37:06 +0100

    fix: serializing parameter to WebAssembly from SSR

--- BlazorApp.Client/Products/ProductInformation.razor.cs
+++ BlazorApp.Client/Products/ProductInformation.razor.cs
@@ -1,20 +1,28 @@
-﻿using Shared.Products;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Shared.Products;
 
 namespace BlazorApp.Client.Products;
 
 public partial class ProductInformation
 {
     [Parameter]
-    public Product Product { get; set; }
+    public string Product { get; set; }
+
+    private Product _product;
 
     [Inject]
     public IToastService ToastService { get; set; }
 
+    protected override void OnParametersSet()
+    {
+        _product = JsonSerializer.Deserialize<Product>(Product);
+    }
+
     private Task Buy()
     {
-        ToastService.ShowSuccess($"{Product.Name} added to cart");
+        ToastService.ShowSuccess($"{_product.Name} added to cart");
         return Task.CompletedTask;
     }
 }
